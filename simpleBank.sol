7beb5528a6e8dfffd6f7293d67b6269fa025da04
0guztumay <oguz.tumay@hotmail.com>
2022-08-11 23:11:27 +0300

    Simple Bank

---
+++ simpleBank.sol
@@ -0,0 +1,23 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+contract simpleBank{
+    //Get Money
+    function getMoney()public payable{
+    }
+    //Show Account Balance
+    function getBalance()public view returns(uint){
+        return address(this).balance;
+    }
+    //Send Money
+    uint256 x18 =1000000000000000000;
+    function sendMoney(address payable _address, uint _amount)public payable returns(uint balance){
+        require(_amount*x18 < address(this).balance, "You don't have enough money");
+        _address.transfer(_amount*x18);
+   }
+   //Refund
+   function refund()public{
+       address payable to= payable(msg.sender);
+       to.transfer(getBalance());
+   }
+}
