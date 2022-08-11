afe08de1ec8108f38deb2d35f459da3c3ddafa78
0guztumay <oguz.tumay@hotmail.com>
2022-08-12 00:20:26 +0300

    updated

--- simpleBank.sol
+++ simpleBank.sol
@@ -20,12 +20,4 @@ contract simpleBank{
        address payable to= payable(msg.sender);
        to.transfer(getBalance());
    }
-   uint sayi1;
-   uint sayi2;
-   uint sonuc;
-   function islem(uint _sayi1,uint _sayi2)public{
-    sayi1=_sayi1;
-    sayi2=_sayi2;
-    sonuc=sayi1+sayi2;
-   }
 }
