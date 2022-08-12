a80e78d7de2987621ab121f8c140fddadd9a6605
0guztumay <oguz.tumay@hotmail.com>
2022-08-12 18:33:29 +0300

    updated

--- simpleBank.sol
+++ simpleBank.sol
@@ -9,13 +9,13 @@ contract simpleBank{
     function getBalance()public view returns(uint){
         return address(this).balance;
     }
-    //Send Money
+    //Send Money || Money Transfer Between Bank Accounts(Addresses)
     uint256 x18 =1000000000000000000;
     function sendMoney(address payable _address, uint _amount)public payable returns(uint balance){
         require(_amount*x18 < address(this).balance, "You don't have enough money");
         _address.transfer(_amount*x18);
    }
-   //Refund
+   //Full Refund
    function refund()public{
        address payable to= payable(msg.sender);
        to.transfer(getBalance());
